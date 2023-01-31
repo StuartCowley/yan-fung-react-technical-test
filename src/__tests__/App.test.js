7aa94107d8b16ba0ec49b66214839f28d95d7275
Yan Fung <fungmingyan@hotmail.com>
2023-01-31 17:24:47 +0000

    Added test to check the component being rendered correctly

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
 import App from "../components/App";
 
-test("renders learn react link", () => {
-  render(<App />);
+describe("App", () => {
+  const { asFragment } = render(<App />);
+
+  it("renders correctly", () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
