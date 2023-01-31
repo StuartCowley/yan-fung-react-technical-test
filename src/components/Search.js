23343ad603f67c2051105bf55f7c36bcb89d24ad
Yan Fung <fungmingyan@hotmail.com>
2023-01-31 17:03:12 +0000

    Created Search component with input field being returned

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "../styles/search.css";
+
+const Search = () => {
+  return (
+    <>
+      <input className="user-input" type="text" />
+    </>
+  )
+}
+
+export default Search;
