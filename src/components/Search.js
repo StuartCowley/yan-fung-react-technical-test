3646949f6666d3405bdd4e083cdf95f98dfaee7f
Yan Fung <fungmingyan@hotmail.com>
2023-02-01 14:19:25 +0000

    Added setValue state variable, className and type for input and button

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,18 @@ import React, { useState } from "react";
 import "../styles/search.css";
 
 const Search = () => {
+  const [value, setValue] = useState()
+
   return (
     <>
-      <input className="user-input" type="text" />
+      <form className="search-form">
+        <input 
+          className="search-input" 
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="submit-button" type="submit">GO!</button>
+      </form>
     </>
   )
 }
