d8071d2138533dff0de60d5ea775c36072d95b13
Yan Fung <fungmingyan@hotmail.com>
2023-02-01 22:44:17 +0000

    Modified the function to display images by searching

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "../styles/search_results.css"
 
-const SearchResults = ({ searchResults }) => {
-  return (
-    <>
-      <p>
-        Search Results
-      </p>
-      {searchResults?.map((e) => console.log(e))}
-    </>
-  )
+const SearchResults = ({ results }) => {
+  if (!results?.length) {
+    console.log(results)
+    return <p>No results</p>
+  } else {
+    return (
+      <div className="search-results">
+        {results.map((image, index) => {
+          return (
+            <img 
+              className="search-results__image" 
+              src={image} 
+              alt="space_image" 
+              key={index}
+            />
+          )
+        })}
+      </div>
+    )
+  }
 }
-
 export default SearchResults;
