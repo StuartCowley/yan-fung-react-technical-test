14d161a07f1d623e10c598631f8b88cbe9e50346
Yan Fung <fungmingyan@hotmail.com>
2023-02-01 16:46:18 +0000

    Add searchResults state variable and passed setSearchResults into Search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./Search";
 import nasaImage from "../styles/Images/nasa-logo.jpeg";
 import "../styles/app.css";
 
 function App() {
+  const [searchResults, setsearchResults] = useState();
+  
   return (
     <div className="app">
       <img className="nasa-logo"
         src={nasaImage}
         alt="nasa" 
       />
-      <Search />
+      <Search setsearchResults={setsearchResults} />
     </div>
   );
 }
