129faafcec8b9f22ab23cae3ed73633be0f0855a
Yan Fung <fungmingyan@hotmail.com>
2023-02-01 21:28:21 +0000

    Added map menthod to the searchResults array

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -7,7 +7,7 @@ const SearchResults = ({ searchResults }) => {
       <p>
         Search Results
       </p>
-      <img className="moon-image" src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="moon" />
+      {searchResults?.map((e) => console.log(e))}
     </>
   )
 }
