ad02e22c423b16f8267de9b4c944cb79be8caf82
Yan Fung <fungmingyan@hotmail.com>
2023-02-03 08:26:46 +0000

    Removed TextDecoder from import

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,5 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 
-import { TextEncoder, TextDecoder } from 'util';
+import { TextEncoder } from "util";
 global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
