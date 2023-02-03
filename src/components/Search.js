33f48d6586afe6d517d0d0df7a06ce521d562d8c
Yan Fung <fungmingyan@hotmail.com>
2023-02-03 08:23:50 +0000

    Added propTypes and made minor formatting changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 import getImages from "../requests/getImages";
 import "../styles/search.css";
 
@@ -8,20 +9,26 @@ const Search = ({ setSearchResults }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setSearchResults(await getImages(value));
-  }
+  };
 
   return (
     <>
       <form className="search-form" onSubmit={handleSubmit}>
-        <input 
-          className="search-input" 
+        <input
+          className="search-input"
           type="text"
           onChange={(e) => setValue(e.target.value)}
         />
-        <button className="submit-button" type="submit">GO!</button>
+        <button className="submit-button" type="submit">
+          GO!
+        </button>
       </form>
     </>
-  )
-}
+  );
+};
+
+Search.propTypes = {
+  setSearchResults: PropTypes.func.isRequired,
+};
 
 export default Search;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,24 +1,30 @@
 import React from "react";
-import "../styles/search_results.css"
+import PropTypes from "prop-types";
+import "../styles/search_results.css";
 
 const SearchResults = ({ results }) => {
   if (!results?.length) {
-    return <p>No results</p>
+    return <p>No results</p>;
   } else {
     return (
       <div className="search-results">
         {results.map((image, index) => {
           return (
-            <img 
-              className="search-results__image" 
-              src={image} 
-              alt="space_image" 
+            <img
+              className="search-results__image"
+              src={image}
+              alt="space_image"
               key={index}
             />
-          )
+          );
         })}
       </div>
-    )
+    );
   }
-}
+};
+
+SearchResults.propTypes = {
+  results: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
 export default SearchResults;
