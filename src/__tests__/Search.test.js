d87be03ca1fd93666f1d69db1c1c522859e2513f
Yan Fung <fungmingyan@hotmail.com>
2023-02-03 08:18:31 +0000

    Added validProps to pass to the component for testing

--- src/__tests__/Search.test.js
+++ src/__tests__/Search.test.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Search from "../components/Search";
 
 describe("Search", () => {
-  const { asFragment } = render(<Search />);
+  const validProps = {
+    setSearchResults: jest.fn()
+  }
 
   it("renders correctly", () => {
+    const { asFragment } = render(<Search setSearchResults={validProps.setSearchResults} />);
+
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("button displays the correct text", () => {
+    render(<Search setSearchResults={validProps.setSearchResults} />)
+
+    const button = screen.getAllByRole("button");
+
+    expect(button).toHaveLength(1);
   })
-})
+});
